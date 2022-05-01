bc9cfbb6b298b2a9ace43fad6f6a77cae62a24f4
lunetis <lunetis@naver.com>
2022-05-01 22:12:51 +0900

    이미지 폴더 임포트 기능 구현
    - 선택한 폴더에서 png, jpg 파일을 가져와 텍스처로 보여주는 기능 구현

--- Assets/Scripts/PresentationController.cs
+++ Assets/Scripts/PresentationController.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using SFB;
 using System.IO;
+using TMPro;
+using System.Linq;
 
 public class PresentationController : MonoBehaviour
 {
+    [SerializeField]
+    MeshRenderer meshRenderer;
+    [SerializeField]
+    TextMeshProUGUI slideText;
+
+    [SerializeField]    
+    bool preloadTextures = false;
+
+    // Don't use preload textures: Load when showing other slides
+    string[] imagePaths;
+    // Use preload textures: Load when import folder
+    Texture2D[] textures;
+
+    int index = 0;
+    int maxIndex = -1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        slideText.text = "Press \"Open Folder\" to import";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ImportFolder()
     {
@@ -28,17 +41,86 @@ public class PresentationController : MonoBehaviour
             path += str;
         }
 
-        Debug.Log(path);
-        GetImageFromFolder(path);
+        // Not selected (ex. cancel)
+        if(path == "")
+        {
+            return;
+        }
+
+        imagePaths = GetImagesFromFolder(path).ToArray<string>();
+        Debug.Log(imagePaths.Length + " png images were found.");
+
+        // No images found
+        if(imagePaths.Length == 0)
+        {
+            maxIndex = -1;
+            return;
+        }
+
+        maxIndex = imagePaths.Length - 1;
+        index = 0;
+
+        if(preloadTextures == true)
+        {
+            textures = new Texture2D[maxIndex + 1];
+            for(int i = 0; i < imagePaths.Length; i++)
+            {
+                textures[i] = GetTextureFromImage(imagePaths[i]);
+            }
+        }
+
+        ShowSlide();
     }
 
-    void GetImageFromFolder(string folderPath)
+    public void ShowNextSlide()
     {
-        var imagesToLoad = Directory.GetFiles(folderPath, "*.png");
+        index = Mathf.Clamp(index + 1, 0, maxIndex);
+        ShowSlide();
+    }
 
-        foreach(string imagePath in imagesToLoad)
+    public void ShowPrevSlide()
+    {
+        index = Mathf.Clamp(index - 1, 0, maxIndex);
+        ShowSlide();
+    }
+
+    void ShowSlide()
+    {
+        if(maxIndex == -1)
         {
-            Debug.Log(imagePath);
+            return;
+        }
+
+        if(preloadTextures == true)
+        {
+            meshRenderer.material.mainTexture = textures[index];
+        }
+        else
+        {
+            meshRenderer.material.mainTexture = GetTextureFromImage(imagePaths[index]);
+        }
+        slideText.text = string.Format("Slide {0} / {1}", index + 1, maxIndex + 1);
+    }
+
+    IEnumerable<string> GetImagesFromFolder(string folderPath)
+    {
+        // Filter: png, jpg
+        var paths = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.AllDirectories);
+        return paths.Where(s => s.EndsWith(".png") || s.EndsWith(".jpg"));
+    }
+
+    Texture2D GetTextureFromImage(string imagePath)
+    {
+        Texture2D texture;
+        if(File.Exists(imagePath))
+        {
+            texture = new Texture2D(2, 2);
+            texture.LoadImage(File.ReadAllBytes(imagePath));
+            return texture;
+        }
+        else
+        {
+            return null;
         }
     }
 }
